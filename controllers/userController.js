c3be5c8551ccefbceb90e924afed007987f35481
5h140w <5h140w23@gmail.com>
2024-07-05 20:49:53 +0100

    controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,6 @@
 const user = require("../models/user");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 // method : GET
 // @localhost:8000/user/allusers
@@ -12,23 +13,27 @@ module.exports.getAllusers = async (req, res) => {
 // @ localhost:8000/user/add
 module.exports.adduser = async (req, res) => {
   // req.body
-  const { name, email, password, isActive } = req.body;
+  try {
+    const { name, email, password, isActive } = req.body;
 
-  // 9adeh min mara bech na3mel hashage
-  const salt = await bcrypt.genSalt(10);
-  // a3tini text => result
-  const hashedpassword = await bcrypt.hash(password, salt);
+    // 9adeh min mara bech na3mel hashage
+    const salt = await bcrypt.genSalt(10);
+    // a3tini text => result
+    const hashedpassword = await bcrypt.hash(password, salt);
 
-  const addeduser = new user({
-    name,
-    // field email : email miniscule
-    email: email.toLowerCase(),
-    password: hashedpassword,
-    isActive,
-  });
+    const addeduser = new user({
+      name,
+      // field email : email miniscule
+      email: email.toLowerCase(),
+      password: hashedpassword,
+      isActive,
+    });
 
-  await addeduser.save();
-  return res.status(200).json(addeduser);
+    await addeduser.save();
+    return res.status(200).json(addeduser);
+  } catch (e) {
+    return res.status(500).json(e.message);
+  }
 };
 
 // PUT
@@ -46,4 +51,37 @@ module.exports.deleteuser = async (req, res) => {
   const { iddelete } = req.params;
   await user.findByIdAndDelete(iddelete);
   return res.status(200).json("deleted!");
+};
+
+// POST
+// @localhost:8000/user/login
+module.exports.login = async (req, res) => {
+  try {
+    //req.body
+    const { email, password } = req.body;
+    const loginuser = await user.findOne({ email: email });
+    // if none
+    if (!loginuser) {
+      return res.status(401).send("no user with this email");
+    }
+    // kan l9it
+    // check password
+    const isMatch = await bcrypt.compare(password, loginuser.password);
+    if (!isMatch) {
+      return res.status(400).send("invalid credentials");
+    }
+
+    const token = jwt.sign(
+      //payload
+      {
+        id: loginuser._id,
+      },
+      "XXXXXXXXX", //jwt_secret
+      { expiresIn: "1h" }
+    );
+
+    return res.status(200).json({ token });
+  } catch (e) {
+    return res.status(500).send(e.message);
+  }
 };
--- routes/userroute.js
+++ routes/userroute.js
@@ -4,12 +4,14 @@ const {
   adduser,
   activeuser,
   deleteuser,
+  login,
 } = require("../controllers/userController");
 
 const userRoute = express.Router();
 
 userRoute.get("/allusers", getAllusers);
 userRoute.post("/add", adduser);
+userRoute.post("/login", login);
 userRoute.put("/active/:idselected", activeuser);
 userRoute.delete("/delete/:iddelete", deleteuser);
 
